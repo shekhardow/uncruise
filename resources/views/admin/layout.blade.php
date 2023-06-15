23a38ebac0bc9f0f61a228355ad5dfe451dc1e6c
shekhardow <shekhar.designoweb@gmail.com>
2023-06-15 11:15:37 +0530

    code delete

--- resources/views/admin/layout.blade.php
+++
@@ -1,9 +0,0 @@
-@include('admin/common/header')
-@if (@$title != 'Login')
-    @include('admin/common/sidebar')
-    {{-- @include('admin/common/setting') --}}
-@endif
-@section('content')
-
-@show
-@include('admin/common/footer')
